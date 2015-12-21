e3b23966fcad46438b65522a74eb4c6bbd4d9fbb
Jonathon W. Marshall <jonathon.marshall@gmail.com>
2015-12-20 21:24:13 -0500

    updated readme and install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-git clone https://github.com/BNOTIONS/horse.vim.git ~/.horse.vim
+git clone https://github.com/jwmarshall/horse.vim.git ~/.horse.vim
 ln -sf ~/.horse.vim/.vimrc ~/.vimrc
 ln -sf ~/.horse.vim/.vimrc.bundles ~/.vimrc.bundles
 vim +BundleInstall +qall
